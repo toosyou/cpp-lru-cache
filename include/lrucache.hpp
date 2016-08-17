04f73fa74aa16fadf56ba3324eed94552987eb65
toosyou <toosyou70207@gmail.com>
2016-08-18 04:11:39 +0800

    add delete_struct in template with expire() function which will be used to expire the values

--- include/lrucache.hpp
+++ include/lrucache.hpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   lrucache.hpp
  * Author: Alexander Ponomarev
  *
@@ -15,7 +15,7 @@
 
 namespace cache {
 
-template<typename key_t, typename value_t>
+template<typename key_t, typename value_t, typename delete_struct>
 class lru_cache {
 public:
 	typedef typename std::pair<key_t, value_t> key_value_pair_t;
@@ -24,7 +24,12 @@ public:
 	lru_cache(size_t max_size) :
 		_max_size(max_size) {
 	}
-	
+	~lru_cache(){
+		for(list_iterator_t it = _cache_items_list.begin(); it != _cache_items_list.end(); ++it){
+			_delete_struct.expire( (*it).second );
+		}
+	}
+
 	void put(const key_t& key, const value_t& value) {
 		auto it = _cache_items_map.find(key);
 		_cache_items_list.push_front(key_value_pair_t(key, value));
@@ -33,15 +38,18 @@ public:
 			_cache_items_map.erase(it);
 		}
 		_cache_items_map[key] = _cache_items_list.begin();
-		
+
 		if (_cache_items_map.size() > _max_size) {
 			auto last = _cache_items_list.end();
 			last--;
 			_cache_items_map.erase(last->first);
+			//use customize expirer to expire
+			value_t& value_to_be_deleted = (_cache_items_list.back()).second;
+			_delete_struct.expire(value_to_be_deleted);
 			_cache_items_list.pop_back();
 		}
 	}
-	
+
 	const value_t& get(const key_t& key) {
 		auto it = _cache_items_map.find(key);
 		if (it == _cache_items_map.end()) {
@@ -51,22 +59,22 @@ public:
 			return it->second->second;
 		}
 	}
-	
+
 	bool exists(const key_t& key) const {
 		return _cache_items_map.find(key) != _cache_items_map.end();
 	}
-	
+
 	size_t size() const {
 		return _cache_items_map.size();
 	}
-	
+
 private:
 	std::list<key_value_pair_t> _cache_items_list;
 	std::unordered_map<key_t, list_iterator_t> _cache_items_map;
 	size_t _max_size;
+	delete_struct _delete_struct;
 };
 
 } // namespace lru
 
 #endif	/* _LRUCACHE_HPP_INCLUDED_ */
-
